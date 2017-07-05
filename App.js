5ff0fce164a3dcf35463b140c9f86875e579fbb6
Furipity <dragan.stanojevic@hotmail.fr>
2017-07-05 09:39:13 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,12 @@
 import React from 'react';
 import { StyleSheet, Image, Text, View } from 'react-native';
 
+export default class HelloWorldApp extends Component (
+  render() {
+    return(
+      <text>
+        Testage
+      </text>
+    );
+  }
+)
